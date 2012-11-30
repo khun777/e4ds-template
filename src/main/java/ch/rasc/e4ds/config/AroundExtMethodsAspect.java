11cd7cc2ac04792866690ced5c365c695318f050
Ralph Schaer <ralphschaer@gmail.com>
2012-11-30 17:00:39 +0100

    Disable aop stuff

--- src/main/java/ch/rasc/e4ds/config/AroundExtMethodsAspect.java
+++ src/main/java/ch/rasc/e4ds/config/AroundExtMethodsAspect.java
@@ -6,10 +6,9 @@ import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.springframework.stereotype.Component;
 
 @Aspect
-@Component
+//@Component
 public class AroundExtMethodsAspect {
 
 	@Around("@annotation(ch.ralscha.extdirectspring.annotation.ExtDirectMethod)")
--- src/main/java/ch/rasc/e4ds/config/ComponentConfig.java
+++ src/main/java/ch/rasc/e4ds/config/ComponentConfig.java
@@ -2,13 +2,12 @@ package ch.rasc.e4ds.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @ComponentScan(basePackages = { "ch.ralscha.extdirectspring", "ch.rasc.e4ds" })
 @PropertySource({ "version.properties" })
-@EnableAspectJAutoProxy
+//@EnableAspectJAutoProxy
 public class ComponentConfig {
 	// nothing here
 }
