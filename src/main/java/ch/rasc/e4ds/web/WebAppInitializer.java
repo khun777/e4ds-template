b8209af292ae14378b0ab76a38f653f8a836c3d5
Ralph Schaer <ralphschaer@gmail.com>
2013-04-09 07:18:48 +0200

    Remove jackson-datatype-hibernate4. We don't need it in this project.

--- src/main/java/ch/rasc/e4ds/web/WebAppInitializer.java
+++ src/main/java/ch/rasc/e4ds/web/WebAppInitializer.java
@@ -26,7 +26,6 @@ import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.context.support.GenericWebApplicationContext;
-import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.filter.DelegatingFilterProxy;
 import org.springframework.web.servlet.DispatcherServlet;
 
