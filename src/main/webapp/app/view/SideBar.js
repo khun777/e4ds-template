02d071c7c241382d32bb6d6cef943635e5a56430
Ralph Schaer <ralphschaer@gmail.com>
2013-11-05 08:48:17 +0100

    Remove requires. NavigationStore is already required in Viewport

--- src/main/webapp/app/view/SideBar.js
+++ src/main/webapp/app/view/SideBar.js
@@ -1,6 +1,6 @@
 Ext.define('E4ds.view.SideBar', {
 	extend: 'Ext.tree.Panel',
-	requires: [ 'E4ds.view.poll.PollChart', 'E4ds.view.user.List', 'E4ds.view.accesslog.List', 'E4ds.view.logevent.List', 'E4ds.view.config.Edit', 'E4ds.store.Navigation' ],
+	requires: [ 'E4ds.view.poll.PollChart', 'E4ds.view.user.List', 'E4ds.view.accesslog.List', 'E4ds.view.logevent.List', 'E4ds.view.config.Edit' ],
 	title: i18n.navigation,
 	itemId: 'menuTree',
 	collapsible: true,
